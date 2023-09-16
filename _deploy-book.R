367a50befd6d36b67b05b220130d8cebf3aa0d6c
XiangyunHuang <xiangyunfaith@outlook.com>
2023-09-16 14:42:35 +0800

    更新部署代码

--- _deploy-book.R
+++ _deploy-book.R
@@ -1,12 +1,12 @@
 # On CI connect to server, using API KEY and deploy using appId
-rsconnect::addConnectServer("https://bookdown.org", "bookdown.org")
+rsconnect::addServer(url = "https://bookdown.org/", name = "bookdown.org")
 rsconnect::connectApiUser(
   account = "xiangyun", server = "bookdown.org",
   apiKey = Sys.getenv("CONNECT_API_KEY")
 )
-quarto::quarto_publish_site(
-  name = "msg2nd", render = "none",
-  server = "bookdown.org",
-  account = "xiangyun",
-  title = "Modern Statistical Graphics (2nd)"
+rsconnect::deploySite(
+  siteName = "msg2nd",
+  siteTitle = "Modern Statistical Graphics (2nd)",
+  server = "bookdown.org", account = "xiangyun",
+  render = "none", forceUpdate = TRUE
 )
